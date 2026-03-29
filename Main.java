1f2f9dc1c48bdc56aea4238c607abf6dddd85428
Rofiat Olabiyi <g00465381@atu.ie>
2026-03-29 20:16:54 +0100

    main method created and banking system object

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // create the banking system object
+        BankSystem system = new BankSystem();
+
+        // start the main menu
+        system.showMainMenu();
+    }
+}
