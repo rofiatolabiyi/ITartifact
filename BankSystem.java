cef0aa8ea364cc6e30d294e1917a707b8bf3910d
Rofiat Olabiyi <g00465381@atu.ie>
2026-03-29 21:00:47 +0100

    array list and scanner imported and implemented

--- BankSystem.java
+++ BankSystem.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class BankSystem {
+    private ArrayList<BankAccount> accounts;
+    private Scanner scanner;
+
+    public BankSystem() {
+        accounts = new ArrayList<>();
+        scanner = new Scanner(System.in);
+    }
 
 }
