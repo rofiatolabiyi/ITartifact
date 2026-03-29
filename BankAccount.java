1d0146a7a3a84b19676bca41c74f7d8a0801843d
Rofiat Olabiyi <g00465381@atu.ie>
2026-03-29 20:35:27 +0100

    getters

--- BankAccount.java
+++ BankAccount.java
@@ -5,16 +5,31 @@ public class BankAccount {
     private double balance;
 
    // main constructor
-public BankAccount(String accountHolder, int accountNumber, int pin, double balance) {
+    public BankAccount(String accountHolder, int accountNumber, int pin, double balance) {
     this.accountHolder = accountHolder;
     this.accountNumber = accountNumber;
     this.pin = pin;
-    this.balance = balance;
-}
+    this.balance = balance;}
 
-// helper constructor
-public BankAccount(String accountHolder, int accountNumber, int pin) {
-    this(accountHolder, accountNumber, pin, 0.0);
-}
+    // helper constructor
+    public BankAccount(String accountHolder, int accountNumber, int pin) {
+    this(accountHolder, accountNumber, pin, 0.0);}
+
+    //getters
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean checkPin(int enteredPin) {
+        return this.pin == enteredPin;
+    }
 
 }
