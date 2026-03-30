2a6a88b581e20239eaf8181f0231c65618f494c6
Rofiat Olabiyi <g00465381@atu.ie>
2026-03-30 17:45:15 +0100

    multipleAccounts method created

--- BankSystem.java
+++ BankSystem.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class BankSystem {
     private ArrayList<BankAccount> accounts;
     private Scanner scanner;
@@ -9,11 +10,12 @@ public class BankSystem {
         scanner = new Scanner(System.in);
     }
 
+    // MAIN MENU METHOD
     public void showMainMenu() {
         int choice;
 
-        //do while statement
-        do{
+        // do while statement
+        do {
             // display the main menu
             System.out.println("\nWelcome to the Bank System");
             System.out.println("1. Create Account or Multiple Accounts");
@@ -21,12 +23,12 @@ public class BankSystem {
             System.out.println("3. Exit");
             System.out.print("Please select an option: ");
 
-            //read in user input
+            // read in user input
             choice = scanner.nextInt();
             scanner.nextLine(); // buffer
 
-            //switch statement handles choice
-             switch (choice) {
+            // switch statement handles choice
+            switch (choice) {
                 case 1:
                     createMultipleAccounts();
                     break;
@@ -42,4 +44,23 @@ public class BankSystem {
 
         } while (choice != 3);
     }
+
+    // CREATE ACCOUNT METHOD
+    private void createMultipleAccounts() {
+        System.out.println("How many accounts would you like to create?");
+        int count = scanner.nextInt();
+        scanner.nextLine(); // buffer
+
+        // if number isnt valid
+        if(count<=0){
+            System.out.println("Invalid number of accounts.");
+            return;
+        }
+
+        for (int i = 0; i < count; i++) {
+            System.out.println("Creating account " + (i + 1) + ":");
+           createAccount();
+        }
+    }
+
 }
